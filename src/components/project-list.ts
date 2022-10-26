a3da86c932e8bfdebfe980e520a2308adf393888
turalcavad <turaltj@code.edu.az>
2022-10-26 19:30:58 +0400

    webpack configuration

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { Component } from "./base-component.js";
-import { DragStart, Draggable } from "../models/drag-drop.js";
-import { autobind } from "../decorators/autobind.js";
-import { Project, ProjectStatus } from "../models/project.js";
-import { ProjectItem } from "./project-item.js";
-import { projectState } from "../state/project-state.js";
+import { Component } from "./base-component";
+import { DragStart, Draggable } from "../models/drag-drop";
+import { autobind } from "../decorators/autobind";
+import { Project, ProjectStatus } from "../models/project";
+import { ProjectItem } from "./project-item";
+import { projectState } from "../state/project-state";
 export class ProjectList
 	extends Component<HTMLDivElement, HTMLElement>
 	implements DragStart
